28c3a4fe04894290edf4e1815bbbf163fa8bcc95
Debopri-yo <ghoshlipika53@gmail.com>
2025-08-28 21:31:37 +0530

    add color to the calculator and make the digits appear on screen

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let a,b,x;
+let a=0,b=0,x=0,operator;
 function add(a,b){
   return a+b;
 }
-function substract(a,b){
+function subtract(a,b){
   return a-b;
 }
 function multiply(a,b){
@@ -11,8 +11,32 @@ function multiply(a,b){
 function divide(a,b){
   return a/b;
 }
-function operate{
-  if(x=="+"){
-    add()
+function operate(a,b,operator){
+  if(operator==='+'){
+    return add(a,b);
+  }
+  else if(operator==='-'){
+    return substract(a,b);
+  }
+  else if(operator==='*'){
+    return multiply(a,b);
+  }
+  else if(operator==='/'){
+    return divide(a,b);
   }
 }
+const digits = document.querySelectorAll('.digit');
+for(const digit of digits){
+  digit.addEventListener("click",()=>{
+    const screen = document.querySelector('#screen');
+    screen.textContent += digit.textContent;
+    x=screen.textContent;
+  })
+}
+
+const clr=document.getElementById("clr");
+clr.addEventListener('click',function(){
+  const screen = document.querySelector('#screen');
+  screen.textContent = "";
+  a=0;x=0;b=0;operator=0;
+});
